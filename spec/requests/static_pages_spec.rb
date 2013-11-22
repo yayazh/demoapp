6b1ca427615ecce26b819274298167693315b772
Yaya Zhang <yayazh@comcast.net>
2013-11-22 03:14:20 +0000

    Made new static pages controller

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+ 
+describe "Static pages" do
+  describe "Home page" do
+    it "should have the content 'Demo App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Demo App')
+    end
+  end
+  
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end   
+end
